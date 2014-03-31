49f7201a34bbd275e4632e2b8e6309d06a85cfbd
Paul@paul-h.de <paul@paul-h.de>
2014-03-31 16:36:26 +0200

    Added Loading of GPexPermissionNodes into ChildablePermissions

--- src/de/paulomart/gpex/permissible/PermissibleGPex.java
+++ src/de/paulomart/gpex/permissible/PermissibleGPex.java
@@ -11,11 +11,13 @@ import org.bukkit.permissions.PermissibleBase;
 import org.bukkit.permissions.Permission;
 
 import de.paulomart.gpex.GPex;
+import de.paulomart.gpex.permissions.GPexPermission;
 import de.paulomart.gpex.permissions.GPexPlayerGroup;
 
 public class PermissibleGPex extends PermissibleBase{
 
 	public static enum PermissionValue {TRUE, FALSE, NOTSET};
+	public static final String DOTREG = "";
 	
 	@Setter
 	@Getter
@@ -34,32 +36,44 @@ public class PermissibleGPex extends PermissibleBase{
 		gpex = GPex.getInstance();
 		this.player = player;
 		personalGroup = gpex.getPlayerGroup(player);
+			
+		for (GPexPermission gpexPermission : personalGroup.getPermissions()){
+				calc(gpexPermission.getPermissionNode(), permissions, gpexPermission.isPositive());
+		}
+	}	
 		
-		//TODO Load permissions form group... somehow..
+	public void calc(String permissionNode, HashMap<String, ChildablePermission> permissions, boolean positive){
+		String[] permission = permissionNode.split(DOTREG);
+		String keyperm = permission[0];
 		
-		HashMap<String, ChildablePermission> permissions2 = new HashMap<String, ChildablePermission>();
-		
-		permissions2.put("b", new ChildablePermission(PermissionValue.TRUE));
-		permissions2.put("a", new ChildablePermission(PermissionValue.TRUE));
-		
-		
-		permissions.put("vipextras", new ChildablePermission(permissions2));
+		ChildablePermission childablePermission = permissions.get(keyperm);
+		if (childablePermission == null){
+			permissions.put(keyperm, new ChildablePermission());
+			childablePermission = permissions.get(keyperm);
+		}
+							
+		if (permission.length != 1){
+			calc(permissionNode.replaceFirst(keyperm+DOTREG, ""), childablePermission.getChildPermissions(), positive);
+		}else{
+			childablePermission.setValue(positive ? PermissionValue.TRUE : PermissionValue.FALSE);
+		}
 	}
-
+	
 	@Override
 	public void recalculatePermissions(){
-		
+
 	}
 
 	private PermissionValue getValue(String permission, HashMap<String, ChildablePermission> permissions, String old){
-		String subPermission = permission.split("\\.")[0];
+		String subPermission = permission.split(DOTREG)[0];
 		ChildablePermission childablePermission = permissions.get(subPermission);
 		if (childablePermission == null){
 			return PermissionValue.NOTSET;
 		}
 				
-		if (childablePermission.hasChilds()){
-			return getValue(old.replaceAll(subPermission+".", ""), childablePermission.getChildPermissions(), old);
+		if (childablePermission.hasChilds()){			
+			String str = old.substring(old.indexOf(subPermission)+subPermission.length()+1, old.length());
+			return getValue(str, childablePermission.getChildPermissions(), old);
 		}
 		return childablePermission.getValue();		
 	}
@@ -68,7 +82,7 @@ public class PermissibleGPex extends PermissibleBase{
 	public boolean hasPermission(String permission){
 		long start = System.currentTimeMillis();
 		boolean ret = getValue(permission, permissions, permission) == PermissionValue.TRUE;
-		System.out.println("Permission Check took: "+ (System.currentTimeMillis() - start));
+		System.out.println("Permission Check took: "+ (System.currentTimeMillis() - start)+ " checked: "+permission);
 		return ret;
 	}
 	
@@ -81,7 +95,14 @@ public class PermissibleGPex extends PermissibleBase{
 	public class ChildablePermission{
 		
 		private HashMap<String, ChildablePermission> childPermissions = new HashMap<String, PermissibleGPex.ChildablePermission>();
+		@Setter
 		private PermissionValue value = PermissionValue.NOTSET;
+		
+		/**
+		 * Use for late init.
+		 */
+		public ChildablePermission(){
+		}
 		
 		public ChildablePermission(HashMap<String, ChildablePermission> childPermissions){
 			this.childPermissions.putAll(childPermissions);
@@ -90,9 +111,18 @@ public class PermissibleGPex extends PermissibleBase{
 		public ChildablePermission(PermissionValue value){
 			this.value = value;
 		}
-		
+				
 		public boolean hasChilds(){
 			return !childPermissions.isEmpty();
+		}
+		
+		@Override
+		public String toString(){
+			if (hasChilds()){
+				return childPermissions.toString();
+			}else{
+				return value.toString();
+			}
 		}
 	}
 }
